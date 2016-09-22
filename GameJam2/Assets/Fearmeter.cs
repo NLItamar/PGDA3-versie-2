b993bdcba563a328db6ada0c3af722b75ea9a3b1
dorrr001 <dorrr001@hva.nl>
2016-09-22 14:15:17 +0200

    rickerd

---
+++ GameJam2/Assets/Fearmeter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fearmeter : MonoBehaviour {
+    public Scrollbar Fearbar;
+    public float Fear = 100;
+
+    public void Feared(float fearvalue)
+    {
+        Fear -= fearvalue;
+        Fearbar.value = Fear / 100f;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
