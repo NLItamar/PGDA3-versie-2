7c13d65ec4ec29d52be175224e607c659026401b
TimRoberson <timroberson@live.nl>
2016-09-22 13:59:53 +0200

    Light Switch

---
+++ GameJam2/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,31 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+        public int count = 0;
+        public bool clicked = false;
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //enabled = !enabled;
+        gameObject.light.intensity = 0; 
+
+        if (clicked)
+        {
+            count++;
+            if (count % 5 == 0 && count < 60)
+            {
+                //this.enabled = !this.enabled;
+            }
+        
+
+
+            if (count >= 60)
+            {
+                count = 0;
+                clicked = false;
+            }
+        }
+	}
+}*/
---
+++ GameJam2/Assets/Scripts/SwitchOffLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchOffLight : MonoBehaviour
+{
+    public Material MaterialOn;
+    public Material MaterialOff;
+    public Light LampLight;
+
+    void FixedUpdate()
+    {
+        MeshRenderer my_renderer = GetComponent<MeshRenderer>();
+
+        if (LampLight.enabled)
+        {
+            my_renderer.material = MaterialOn;
+        }
+        else
+        {
+            my_renderer.material = MaterialOff;
+        }
+    }
+}
